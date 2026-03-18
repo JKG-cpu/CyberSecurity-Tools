3f38ad49e7242e3303a92d4a3dd71af57aa37a90
JKG <jkg66512@gmail.com>
2026-03-18 15:34:09 +0000

    ReconKit /bin refreshed, upload to pypi(?)

--- ReconKit/ReconProcessor/obj/Debug/net10.0/ReconProcessor.AssemblyInfo.cs
+++ ReconKit/ReconProcessor/obj/Debug/net10.0/ReconProcessor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReconProcessor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f725cba7f101ed444a03376fe58b2941016d9a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9b461af3e8662c7777409017cfa8e459d904cd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReconProcessor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReconProcessor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
