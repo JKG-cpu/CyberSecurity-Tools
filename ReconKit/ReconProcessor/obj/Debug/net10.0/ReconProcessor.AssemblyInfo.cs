892938fb2bcc5e7c275870476b239633bde0a9e0
JKG <jkg66512@gmail.com>
2026-03-24 19:53:32 +0000

    Removed some things not needed

--- ReconKit/ReconProcessor/obj/Debug/net10.0/ReconProcessor.AssemblyInfo.cs
+++ ReconKit/ReconProcessor/obj/Debug/net10.0/ReconProcessor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReconProcessor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d383e4eecc1b9721308726a1b3a741def3a3a5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c67844d04f182324896d9ff96d7d4dedbb86e669")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReconProcessor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReconProcessor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
