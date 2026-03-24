d967281dc6e0fa1903b1f2ebaf8ecb4f0b58a5f5
JKG <jkg66512@gmail.com>
2026-03-24 20:11:47 +0000

    Setup Project; finished Day 1-2 (following idea.txt)

---
+++ NetWatch/main.py
@@ -0,0 +1,12 @@
+import argparse
+
+class Main:
+    def __init__(self) -> None:
+        pass
+
+if __name__ == "__main__":
+    m = Main()
+
+    parser = argparse.ArgumentParser("NetWatch")
+
+    args = parser.parse_args()
---
+++ NetWatch/src/helpers/file_handler.py
@@ -0,0 +1,25 @@
+import json
+
+class FileHandler:
+    def load_data(self, file_path: str) -> dict | list:
+        try:
+            with open(file_path, "r") as f:
+                return json.load(f)
+        
+        except FileNotFoundError:
+            print(f"Filepath {file_path} was not found")
+        
+        except Exception as e:
+            print(f"Error loading data: {e}")
+    
+    def save(self, file_path: str, data: dict | list) -> None:
+        try:
+            with open(file_path, "w") as f:
+                json.dump(data, f)
+        
+        except FileNotFoundError:
+            print(f"Filepath {file_path} was not found")
+        
+        except Exception as e:
+            print(f"Error saving data: {e}")
+
