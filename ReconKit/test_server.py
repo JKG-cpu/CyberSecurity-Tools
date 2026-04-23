b414e73f41b7f14712040c9387e8bfb6e8d27475
JKG <jkg66512@gmail.com>
2026-04-23 16:51:19 +0000

    Created .yml files for publishing

--- ReconKit/test_server.py
+++
@@ -1,7 +0,0 @@
-import socket
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind(("localhost", 9999))
-    s.listen()
-    print("Listening on port 9999...")
-    conn, addr = s.accept()
